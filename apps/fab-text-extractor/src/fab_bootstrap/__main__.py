86c35ee2dcd2d4cfa1d14a1dc1838418666bdb4f
GaborZeller <gabriel.d.celery@gmail.com>
2025-05-03 22:06:32 +0100

    feat: extending text extractor with logging and version model downloads

--- apps/fab-text-extractor/src/fab_bootstrap/__main__.py
+++ apps/fab-text-extractor/src/fab_bootstrap/__main__.py
@@ -2,32 +2,58 @@ import os
 import zipfile
 from urllib.request import urlretrieve
 
+from fab_extract.logs import init_logger
+
 
 def main():
+    log_level = os.environ["LOG_LEVEL"]
     models_dir = os.environ["MODELS_DIR"]
+    model_name = os.environ["MODEL_NAME"]
     language_model_url = os.environ["LANGUAGE_MODEL_URL"]
     text_detection_model_url = os.environ["TEXT_DETECTION_MODEL_URL"]
 
-    target_dir_path = os.path.join(models_dir, "model")
+    logger = init_logger(log_level)
+
+    target_dir_path = os.path.join(models_dir, model_name)
 
     os.makedirs(target_dir_path, exist_ok=True)
 
     language_model_file_name = language_model_url.split("/")[-1]
 
+    logger.info(
+        f"retrieving lanugage model",
+        extra={"language_model_url": language_model_url},
+    )
+
     urlretrieve(language_model_url, language_model_file_name)
 
     text_detection_model_file_name = text_detection_model_url.split("/")[-1]
 
+    logger.info(
+        f"retrieving text detection model",
+        extra={"text_detection_model_url": text_detection_model_url},
+    )
+
     urlretrieve(text_detection_model_url, text_detection_model_file_name)
 
     with zipfile.ZipFile(language_model_file_name, "r") as zip_ref:
-        print(f"ZIP file contents: {zip_ref.namelist()}")
-
+        logger.info(
+            f"extract lanugage model",
+            extra={
+                "target_dir_path": target_dir_path,
+                "language_model_file_name": language_model_file_name,
+            },
+        )
         zip_ref.extractall(target_dir_path)
 
     with zipfile.ZipFile(text_detection_model_file_name, "r") as zip_ref:
-        print(f"ZIP file contents: {zip_ref.namelist()}")
-
+        logger.info(
+            f"extract lanugage model",
+            extra={
+                "target_dir_path": target_dir_path,
+                "text_detection_model_file_name": text_detection_model_file_name,
+            },
+        )
         zip_ref.extractall(target_dir_path)
 
 
---
+++ apps/fab-text-extractor/src/fab_bootstrap/store.py
@@ -0,0 +1,21 @@
+import sqlite3
+
+
+def create_table(db_name: str):
+    with sqlite3.connect(db_name) as conn:
+        cur = conn.cursor()
+
+        try:
+            cur.execute(
+                """CREATE TABLE IF NOT EXISTS extracts (
+                card_name TEXT NOT NULL,
+                model_name TEXT NOT NULL,
+                segment INT NOT NULL,
+                text TEXT NOT NULL,
+                PRIMARY KEY (card_name, model_name, segment)
+
+            )"""
+            )
+            pass
+        except:
+            pass
