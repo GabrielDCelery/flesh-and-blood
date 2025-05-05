5e2f1c14bf18f81dbc36b547edec431ca57cfd91
GaborZeller <gabriel.d.celery@gmail.com>
2025-05-05 15:27:01 +0100

    chore: removed redundant archive dir

--- archive/fab-image-segmentation/src/fab_image_segmentation/__main__.py
+++
@@ -1,9 +0,0 @@
-from fab_image_segmentation.card_segmentation import run
-
-
-def main():
-    run()
-
-
-if __name__ == "__main__":
-    main()
--- archive/fab-image-trainer/src/fab_image_trainer/__main__.py
+++
@@ -1,9 +0,0 @@
-from fab_image_trainer.train import run
-
-
-def main():
-    run()
-
-
-if __name__ == "__main__":
-    main()
